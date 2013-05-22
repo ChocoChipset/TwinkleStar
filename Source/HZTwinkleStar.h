07e6287fec4d91756b9bb5064202d291bbc1057a
Hector Zarate <hecktorzr@gmail.com>
2013-05-22 23:19:45 +0200

    Created TwinklStar class

---
+++ Source/HZTwinkleStar.h
@@ -0,0 +1,13 @@
+//
+//  HZTwinkleStar.h
+//  TwinkleStarExample
+//
+//  Created by Hector Zarate on 5/22/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HZTwinkleStar : NSObject
+
+@end
---
+++ Source/HZTwinkleStar.m
@@ -0,0 +1,13 @@
+//
+//  HZTwinkleStar.m
+//  TwinkleStarExample
+//
+//  Created by Hector Zarate on 5/22/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import "HZTwinkleStar.h"
+
+@implementation HZTwinkleStar
+
+@end
