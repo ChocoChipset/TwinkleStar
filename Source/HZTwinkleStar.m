89535e291ef74514f911e08499179ca172805b3b
Hector Zarate <hecktorzr@gmail.com>
2013-05-22 23:42:24 +0200

    Implemented function to obtain period from a given frequency

--- Source/HZTwinkleStar.m
+++ Source/HZTwinkleStar.m
@@ -8,6 +8,29 @@
 
 #import "HZTwinkleStar.h"
 
+
+#pragma mark - Other Functions Headers
+
+NSTimeInterval TimeIntervalForFrequency(CGFloat frequency_in_hz);
+
+
 @implementation HZTwinkleStar
 
 @end
+
+
+#pragma mark - Other Functions
+
+NSTimeInterval TimeIntervalForFrequency(CGFloat frequency_in_hz)
+{
+    /*  period[s] = 1 / frequency[hz]   */
+    
+    if (frequency_in_hz == 0.0)
+    {
+        return DBL_MAX;
+    }
+    
+    NSTimeInterval periodInterval = 1.0 / fabs(frequency_in_hz);
+    
+    return periodInterval;
+}
